d79df2d54a2091c5d04246b6ad6dbeff6e676bee
pattrob <92001075+pattrob@users.noreply.github.com>
2023-08-30 13:40:28 -0400

    Add files via upload

---
+++ infoTest.java
@@ -0,0 +1,14 @@
+public class infoTest {
+    private int infoTest;
+    public static void main(String[] args){
+        int num = 5;
+        while(num < 0){
+            System.out.println("Information void");
+            num--;
+        }
+
+        do{
+
+        }
+    }
+}
